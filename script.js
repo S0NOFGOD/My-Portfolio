41d878a58d44dd08b7f17224f30729f042cff3eb
Destiny Okpone <destiny.okpone@gmail.com>
2025-09-26 07:13:19 +0100

    Added interactive quote feature and styles

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    document.getElementById('projects').innerText = "My Portfolio projects will show up here soon";
-});
+async function loadQuote() {
+  const response = await fetch("https://api.quotable.io/random");
+  const data = await response.json();
+  document.getElementById("projects").innerHTML = `
+    <blockquote>"${data.content}"</blockquote>
+    <p>- ${data.author}</p>
+  `;
+}
+
+loadQuote();
